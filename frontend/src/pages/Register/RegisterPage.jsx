2e0939fb9d7f4e942e54df643d12a4791e1dfe7c
predrag jandric <predrag.jandric.bg@gmail.com>
2023-09-26 23:48:15 +0200

    register page in progress saving work

--- frontend/src/pages/Register/RegisterPage.jsx
+++ frontend/src/pages/Register/RegisterPage.jsx
@@ -75,3 +75,108 @@ function RegisterPage() {
 	const togglePasswordVisibility = () => {
 		setShowPassword(!showPassword);
 	};
+
+	return (
+
+		<main className='w-[90%] mx-auto my-10 gap-8 flex flex-col sm:w-[30rem]'>
+			<h2 className='text-2xl font-semibold text-primary text-center bg-white py-6 border-1 border-blue-300 rounded-5 drop-shadow-lg'>REGISTER</h2>
+
+			<form onSubmit={formik.handleSubmit} className='flex flex-col gap-2 bg-white py-3 px-6 drop-shadow-lg border-1 border-blue-300 rounded-5 '>
+				<label className='mt-1'>
+					First Name:{' '}
+					<span className='text-red-600 text-[14px]'>
+						{showError('firstName')}
+					</span>{' '}
+				</label >
+				<input
+					className='border-1 border-slate-300 py-2 px-7 rounded-5'
+					type='text'
+					placeholder='Insert First Name'
+					name='firstName'
+					value={formik.values.firstName}
+					onChange={formik.handleChange}
+				/>
+
+				<label className='mt-1'>
+					Last Name:{' '}
+					<span className='text-red-600 text-[14px]'>
+						{showError('lastName')}
+					</span>{' '}
+				</label>
+				<input
+					className='border-1 border-slate-300 py-2 px-7 rounded-5'
+					type='text'
+					placeholder='Insert Last Name'
+					name='lastName'
+					value={formik.values.lastName}
+					onChange={formik.handleChange}
+				/>
+
+				<label className='mt-1'>
+					Email:{' '}
+					<span className='text-red-600 text-[14px]'>
+						{showError('email')}
+					</span>{' '}
+				</label>
+				<input
+					className='border-1 border-slate-300 py-2 px-7 rounded-5'
+					type='email'
+					placeholder='Insert Email'
+					name='email'
+					value={formik.values.email}
+					onChange={formik.handleChange}
+				/>
+
+				<label className='mt-1 flex'>
+					Password:{' '}
+					<span className='text-red-600 text-[14px]'>
+						{showError('password')}
+					</span>{' '}
+					<span
+						className='ml-auto hover:underline cursor-pointer px-1'
+						onClick={togglePasswordVisibility}
+					>
+						{showPassword ? "Hide" : "Show"}
+					</span>
+				</label>
+
+				<input
+					className='border-1 border-slate-300 py-2 px-7 rounded-5'
+					type={showPassword ? 'text' : 'password'}
+					placeholder='Insert Password'
+					name='password'
+					value={formik.values.password}
+					onChange={formik.handleChange}
+				/>
+
+				<label className='mt-1'>
+					Gender:{' '}
+					<span className='text-red-600 text-[14px]'>
+						{showError('gender')}
+					</span>{' '}
+				</label >
+				<select className='border-1 border-slate-300 py-2 px-7 rounded-5'
+					name='gender'
+					value={formik.values.gender}
+					onChange={formik.handleChange}>
+					<option value='' defaultChecked>
+						Gender
+					</option>
+					<option value='male'>Male</option>
+					<option value='female'>Female</option>
+				</select>
+
+				<label className='mt-1'>
+					Image:{' '}
+					<span className='text-red-600 text-[14px]'>
+						{showError('image')}
+					</span>{' '}
+				</label>
+				<input
+					className='border-1 border-slate-300 py-2 px-7 rounded-5'
+					type='file'
+					name='image'
+					onChange={(e) => {
+						formik.setFieldValue(e.target.name, e.target.files[0]);
+					}}
+				/>
